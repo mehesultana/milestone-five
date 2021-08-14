0b42f45ca9537d26406f2e87dda5561dea2b34d4
mehesultana <sultanamehe01@gmail.com>
2021-08-15 01:44:16 +0600

    deposit

--- 25-interactive-bank-website/JS/pioneer.js
+++ 25-interactive-bank-website/JS/pioneer.js
@@ -1,7 +1,22 @@
+//login button event handler
 const loginBtn = document.getElementById('login');
 loginBtn.addEventListener('click', function () {
     const loginArea = document.getElementById('login-area');
     loginArea.style.display = 'none';
     const transactionArea = document.getElementById('transation-area');
     transactionArea.style.display = 'block';
+});
+
+//deposit button event handler
+const depositBtn = document.getElementById('addDeposit');
+depositBtn.addEventListener('click', function () {
+    const depositAmount = document.getElementById('depositAmount').value;
+    const depositNumber = parseFloat(depositAmount);
+
+    const currentDeposit = document.getElementById('currentDeposit').innerText;
+    const currentDepositNumber = parseFloat(currentDeposit);
+
+    const totalDeposit = depositNumber + currentDepositNumber;
+
+    document.getElementById('currentDeposit').innerText = totalDeposit;
 });

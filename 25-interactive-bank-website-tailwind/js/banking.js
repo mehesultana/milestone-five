08ae43eaae8e84ad029eed81b79cbb8f8c3107b5
mehesultana <sultanamehe01@gmail.com>
2021-08-15 21:54:30 +0600

    deposit total

--- 25-interactive-bank-website-tailwind/js/banking.js
+++ 25-interactive-bank-website-tailwind/js/banking.js
@@ -30,3 +30,19 @@ document.getElementById('deposit-button').addEventListener('click', function () 
     // clear the deposit input field
     depositInput.value = '';
 });
+
+// handle withdraw event handler
+document.getElementById('withdraw-button').addEventListener('click', function () {
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawAmountText = withdrawInput.value;
+    const newWithdrawAmount = parseFloat(withdrawAmountText);
+    console.log(newWithdrawAmount);
+
+    // update balance
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceText);
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+
+    balanceTotal.innerText = newBalanceTotal;
+});

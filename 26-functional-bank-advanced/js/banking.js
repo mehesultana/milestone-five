be9216ed0fca2e6e0d0ce216dd78ea4ac80fb961
mehesultana <sultanamehe01@gmail.com>
2021-08-16 02:21:43 +0600

    total deposit

--- 26-functional-bank-advanced/js/banking.js
+++ 26-functional-bank-advanced/js/banking.js
@@ -8,10 +8,16 @@ document.getElementById('deposit-button').addEventListener('click', function () 
     //get current deposit
     const depositTotal = document.getElementById('deposit-total');
     const depositTotalText = depositTotal.innerText;
-    const previousDepositTotal = parseFloat(depositTotal);
+    const previousDepositTotal = parseFloat(depositTotalText);
 
-    depositTotal.innerText = depositTotalText + depositAmount;
-    console.log(depositTotalText);
+    depositTotal.innerText = previousDepositTotal + depositAmount;
+    // console.log(depositTotalText);
+
+    //update balance
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    balanceTotal.innerText = previousBalanceTotal + depositAmount;
 
     //clear input field
     depositInput.value = '';

fe38d951daf163e8cfc1553cc2c268ada963cb27
mehesultana <sultanamehe01@gmail.com>
2021-08-18 02:19:28 +0600

    case

--- 27-revisit-javascript&work-on-shopping-cart/shopping-cart/app.js
+++ 27-revisit-javascript&work-on-shopping-cart/shopping-cart/app.js
@@ -1,7 +1,24 @@
-document.getElementById('case-plus').addEventListener('click', function () {
-    // console.log('case plus clicked');
+function updateCaseNumber(isIncreasing) {
     const caseInput = document.getElementById('case-number');
     const caseNumber = caseInput.value;
     // console.log(caseNumber);
-    caseInput.value = parseFloat(caseNumber) + 1;
+
+    if (isIncreasing) {
+        caseInput.value = parseFloat(caseNumber) + 1;
+    } else {
+        caseInput.value = parseFloat(caseNumber) - 1;
+    }
+}
+
+document.getElementById('case-plus').addEventListener('click', function () {
+    // console.log('case plus clicked');
+    updateCaseNumber(true);
+});
+
+document.getElementById('case-minus').addEventListener('click', function () {
+    /* const caseInput = document.getElementById('case-number');
+    const caseNumber = caseInput.value;
+    
+ */
+    updateCaseNumber(false);
 });

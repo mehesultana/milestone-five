f8d78a3ac51421e90b9fc1efe25805c355f1320f
mehesultana <sultanamehe01@gmail.com>
2021-08-19 03:43:10 +0600

    arrguments

--- 28-string&array-useful-methods/arguments.js
+++ 28-string&array-useful-methods/arguments.js
@@ -9,3 +9,13 @@ function addNumber(num1, num2) {
 }
 const sum = addNumber(23, 13, 53, 634);
 console.log(sum);
+
+function getFullName(firstName, lastName) {
+    let fullName = '';
+    for (const part of arguments) {
+        fullName = fullName + part + ' ';
+    }
+    return fullName;
+}
+const name = getFullName('Omuk', 'Songket', 'bin', 'Hanif', 'Songket', 'bin', 'Tomuk', 'Songket', 'bin', 'Kumuk', 'Songket');
+console.log(name);

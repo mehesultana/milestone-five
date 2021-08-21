bf6041cf75bb9494a5ae4b054964d6bd9253ef32
mehesultana <sultanamehe01@gmail.com>
2021-08-22 02:02:17 +0600

    function

---
+++ 29.5-integrate-javascript-bonus-content/js/functional.js
@@ -0,0 +1,11 @@
+function getInputValue(fieldId) {
+    const inputField = document.getElementById(fieldId);
+    const valueInText = inputField.value;
+    const value = parseFloat(valueInText);
+    inputField.value = '';
+    return value;
+}
+
+document.getElementById('deposit-button').addEventListener('click', function () {
+    const amount = getInputValue('deposit-input');
+});

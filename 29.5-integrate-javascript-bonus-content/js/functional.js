79de938759caa25cf9e448cbe285daeaa6949638
mehesultana <sultanamehe01@gmail.com>
2021-08-22 20:54:13 +0600

    functional

--- 29.5-integrate-javascript-bonus-content/js/functional.js
+++ 29.5-integrate-javascript-bonus-content/js/functional.js
@@ -6,39 +6,55 @@ function getInputValue(fieldId) {
     return value;
 }
 
+function getInnerTextValue(fieldId) {
+    const fieldTag = document.getElementById(fieldId);
+    const fieldValueInText = fieldTag.innerText;
+    const value = parseFloat(fieldValueInText);
+    return value;
+}
+
 function updateTotal(fieldId, amount) {
     //console.log(fieldId, amount);
-    const totalTag = document.getElementById(fieldId);
-    const previousTotalInText = totalTag.innerText;
-    const previousTotal = parseFloat(previousTotalInText);
+    // const totalTag = document.getElementById(fieldId);
+    // const previousTotalInText = totalTag.innerText;
+    // const previousTotal = parseFloat(previousTotalInText);
+    const previousTotal = getInnerTextValue(fieldId);
     const newTotal = previousTotal + amount;
-    totalTag.innerText = newTotal;
+    document.getElementById(fieldId).innerText = newTotal;
+    return newTotal;
 }
 
 function updateBalance(amount, isAdding) {
-    const balanceTag = document.getElementById('balance-total');
-    const balanceInText = balanceTag.innerText;
-    const previousBalance = parseFloat(balanceInText);
+    // const balanceTag = document.getElementById('balance-total');
+    // const balanceInText = balanceTag.innerText;
+    // const previousBalance = parseFloat(balanceInText);
+    const previousBalance = getInnerTextValue('balance-total');
     let newBalance;
     if (isAdding == true) {
         newBalance = previousBalance + amount;
     } else {
         newBalance = previousBalance - amount;
     }
-    balanceTag.innerText = newBalance;
+    document.getElementById('balance-total').innerText = newBalance;
 }
 
 document.getElementById('deposit-button').addEventListener('click', function () {
     const amount = getInputValue('deposit-input');
     // console.log(amount);
-    updateTotal('deposit-total', amount);
-    updateBalance(amount, true);
+
+    if (amount > 0) {
+        updateTotal('deposit-total', amount);
+        updateBalance(amount, true);
+    }
 });
 
 //handle withdraw
 document.getElementById('withdraw-button').addEventListener('click', function () {
     const amount = getInputValue('withdraw-input');
     // console.log(amount);
-    updateTotal('withdraw-total', amount);
-    updateBalance(amount, false);
+    const balance = getInnerTextValue('balance-total');
+    if (amount > 0 && amount <= balance) {
+        updateTotal('withdraw-total', amount);
+        updateBalance(amount, false);
+    }
 });
